a4dd3fa9dd21a06277cb1fb3c14aa9490b36feac
felix <felixvjr0@gmail.com>
2024-10-06 22:46:14 -0400

    Adding Second challenge

---
+++ sqlchallenges/challenge2/challenge2.md
@@ -0,0 +1,1 @@
+Challenge 2 is to test out my simple filtering data. I still am working on filtering data in other ways and will incorporate them in future challenges.
---
+++ sqlchallenges/challenge2/challenge2.sql
@@ -0,0 +1,7 @@
+-- Challenge 2
+
+-- How many payments where made by the customer with customer_id = 100?
+SELECT COUNT(*) FROM payment WHERE customer_id = 100; 
+
+-- What is the last name of our customer with first name = "Erica"?
+SELECT last_name FROM customer WHERE first_name = 'ERICA';
