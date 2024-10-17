428ecfa9021496ecd1bf2641eab224487fdbd95f
felix <felixvjr0@gmail.com>
2024-10-17 17:02:05 -0400

    Adding DISTINCT to remove duplicate data

---
+++ distinctsql/distinctsql.md
@@ -0,0 +1,1 @@
+Using the "DISTINCT" allows me to pull data and remove the duplicate values if that is what we intend to do.
---
+++ distinctsql/distinctsql.sql
@@ -0,0 +1,15 @@
+-- DISTINCT to remove duplicates
+
+-- names only occurs once
+SELECT DISTINCT first_name FROM public.actor;
+
+
+SELECT * FROM public.film;
+-- Seeing all the ratings in the column
+SELECT DISTINCT rating FROM public.film;
+
+-- disctinctly show difference in rental duration.
+SELECT DISTINCT rating, rental_duration FROM public.film;
+
+-- Task: Get the prices of payments from high to low.
+SELECT DISTINCT amount FROM public.payment ORDER BY amount DESC;
